b584e48409af34e7ae80e912816946edb325c0dc
Mahmoud Mabrouk <mabrouk.dev.pro@gmail.com>
2023-03-14 07:21:28 +0200

    rest

--- dpk.js
+++ dpk.js
@@ -4,13 +4,14 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate = "0";
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = JSON.stringify(event.partitionKey);
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!event) {
+    return candidate;
+  }
+  if (event.partitionKey) {
+    candidate = JSON.stringify(event.partitionKey);
+  } else if (event) {
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
